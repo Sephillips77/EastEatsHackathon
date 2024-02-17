f4e848a42beb03f2409668e25549e9dc61046b6a
Sephillips77 <146801379+Sephillips77@users.noreply.github.com>
2024-02-17 12:01:50 -0800

    Update header.h

--- header.h
+++ header.h
@@ -1,1 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
+
+
+
+
+typedef struct data
+{
+	char name[25];
+	char category[15];
+	char product[25];
+	double price;
+	char location[15];
+	char seller[25];
+
+}Data;
