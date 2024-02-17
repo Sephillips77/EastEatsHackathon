25b283782374cec310dac56782f91f267006782b
Sephillips77 <146801379+Sephillips77@users.noreply.github.com>
2024-02-17 12:54:57 -0800

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,1 +1,48 @@
 #include "header.h"
+
+
+void main_menu(void)
+{
+	int choice = 0;
+	do
+	{
+		system("cls");
+		printf("\t\t\t\t\tCoug Connects\n");
+		printf("[1] Find a Service\n\n");
+		printf("[2] Create a Service\n\n");
+		printf("[3] About us\n\n");
+		printf("[4] Exit Application\n\n");
+		scanf("%d", &choice);
+
+	} while (choice != 4);
+
+	system("cls");
+	printf("\t\t\t\t\tSee you next time!\n\n");
+
+	return;
+}
+
+int Find_a_Service(Node** pList)
+{
+	int success = 0;
+
+
+	return success;
+}
+
+int Create_a_Service(Node** pList)
+{
+	int success = 0;
+
+
+	return success;
+}
+
+void about_us(void)
+{
+
+
+
+
+	return 0;
+}
