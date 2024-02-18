b880c17fed71e3bb530b7ec8a394a8d5f1988987
Sephillips77 <146801379+Sephillips77@users.noreply.github.com>
2024-02-17 22:21:45 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include "header.h"
 
 int main(void) {
-	 
-	const char* services[4] = { "Food", "Style", "Entertainment", "Other" };     
-	const char* location[3] = { "North Campus", "Central Campus", "South Campus" };      
+
+	const char* services[4] = { "Food", "Style", "Entertainment", "Other" };
+	const char* location[3] = { "North Campus", "Central Campus", "South Campus" };
 
 	Node* pHead = NULL;
 
-	readFile(&pHead); 
+	readFile(&pHead);
 
-	Find_a_Service(&pHead, services, location); 
+	main_menu(pHead, services, location);
+	
 
-//	int success = Find_a_Service(&pHead, services, location); 
+	//	int success = Find_a_Service(&pHead, services, location); 
 
-//	Create_a_Service(&pHead);  
+	//	Create_a_Service(&pHead);  
 
-//	deleteService(&pHead);    
+	//	deleteService(&pHead);    
 
 	return 0;
 }
