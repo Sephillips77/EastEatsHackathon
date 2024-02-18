4239461f8072ac0189812a2830ed7a6d62ba7458
kenson05 <146804011+kenson05@users.noreply.github.com>
2024-02-17 20:30:09 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include "header.h"
 
-int main(void)
-{
-  
-const char* services[4] = { "Food", "Style", "Entertainment", "Other" };
-const char* location[3] = { "North Campus", "Central Campus", "South Campus" };
+int main(void) {
+	 
+	const char* services[4] = { "Food", "Style", "Entertainment", "Other" };     
+	const char* location[3] = { "North Campus", "Central Campus", "South Campus" };      
 
-  return 0;
+	Node* pHead = NULL;
+
+	readFile(&pHead); 
+
+	Find_a_Service(&pHead, services, location); 
+
+//	int success = Find_a_Service(&pHead, services, location); 
+
+//	Create_a_Service(&pHead);  
+
+//	deleteService(&pHead);    
+
+	return 0;
 }
